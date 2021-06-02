deb13aaf84f7c4daa7de4f75bf28ba89eb01a690
RushikeshGadkar906 <gadkarrushikesh906@gmail.com>
2021-06-02 13:49:52 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Text>h2</Text>
+    </View>
+  )
+}
